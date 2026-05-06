96bfb5d620e83cc625a395a6673a27988cc2bbb7
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2026-05-06 18:10:15 +0000

    Implement refresh logic for local Reitanlage documents to auto-link related entries

--- starrain/starrain/doctype/reitanlage/reitanlage.js
+++ starrain/starrain/doctype/reitanlage/reitanlage.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2026, Beveren Software and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Reitanlage", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Reitanlage", {
+	refresh(frm) {
+		if (frm.doc.__islocal) {
+			const last_doc = frappe.contacts.get_last_doc(frm);
+			if (
+				frappe.dynamic_link &&
+				frappe.dynamic_link.doc &&
+				frappe.dynamic_link.doc.name == last_doc.docname
+			) {
+				frm.set_value("links", "");
+				frm.add_child("links", {
+					link_doctype: frappe.dynamic_link.doctype,
+					link_name: frappe.dynamic_link.doc[frappe.dynamic_link.fieldname],
+				});
+				frm.refresh_field("links");
+			}
+		}
+	},
+});
